e3d1e9ca7a4dcf1da376eab4f3ae31858b8b622d
m-yash <61091300+m-yash@users.noreply.github.com>
2020-04-07 20:22:31 +0530

    Add files via upload

---
+++ EG_FetchFromDB.py
@@ -0,0 +1,26 @@
+import pymysql
+
+#create connection
+#add your inputs as ("hostname","username","password","database name")
+con = pymysql.connect("","","","") 
+
+#create object
+cur = con.cursor()
+
+#execute command
+exe = cur.execute("select firstname, lastname from student where id = 1")
+
+data = cur.fetchall()
+
+#here the output will be in a tuple like ('firstname','lastname')
+print(data)  
+
+#use the index method
+for i in data:  
+    firstname = i[0]
+    lastname = i[1]
+
+print(firstname,lastname)
+con.commit()
+con.close()
+
